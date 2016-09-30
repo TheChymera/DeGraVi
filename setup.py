a3b9122c5dfad094bfba10a3b77d7d2952b57439
Horea Christian <horea.christ@yandex.com>
2016-09-30 06:32:19 +0200

    pip installation files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="DeGraVi",
+	version="",
+	description = "Dependency Graph Visualization Tool",
+	author = "Horea Christian",
+	author_email = "horea.christ@yandex.com",
+	url = "https://github.com/TheChymera/DeGraVi",
+	keywords = ["dependency", "package management", "visualization", "graph"],
+	classifiers = [],
+	install_requires = [],
+	provides = ["degravi"],
+	packages = ["degravi"],
+	include_package_data=True,
+	)
