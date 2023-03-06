549a1b1f5d39fc3c6733133da3af0a433539ab5b
Horea Christian <chr@chymera.eu>
2023-03-06 03:38:50 -0500

    Using pytest fixture for testing

--- degravi/tests/test_plot.py
+++ degravi/tests/test_plot.py
@@ -1,6 +1,8 @@
 import pytest
+import os
 
 from ..plot import package_deps
 
-def test_package_deps():
-	package_deps('dev-python/heudiconv')
+def test_package_deps(tmp_path):
+	save_as=os.path.join(str(tmp_path),"{package_name}_mindeps.pdf")
+	package_deps('dev-python/heudiconv', save_as=save_as)
